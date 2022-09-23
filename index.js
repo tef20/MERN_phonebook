821bdf3762c4866b2a711af0ca077847c5d3d0a4
Chris Telford <christelford@hotmail.co.uk>
2022-09-23 14:05:30 -0700

    3.2 /info GET request returns some basic info

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
 const express = require("express");
 const app = express();
 
-const persons = {
-  persons: [
-    {
-      name: "Ada Lovelace",
-      number: "39-44-5323523",
-      id: 2,
-    },
-    {
-      name: "Dan Abramov",
-      number: "12121",
-      id: 3,
-    },
-    {
-      name: "Mary Poppendieck",
-      number: "39-23-6423122",
-      id: 4,
-    },
-    {
-      name: "aa",
-      number: "bb",
-      id: 5,
-    },
-  ],
-};
-
+const persons = [
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2,
+  },
+  {
+    name: "Dan Abramov",
+    number: "12121",
+    id: 3,
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4,
+  },
+  {
+    name: "aa",
+    number: "bb",
+    id: 5,
+  },
+];
 app.get("/", (request, response) => {
   response.send("<h1>Hello</h1>");
+});
+
+app.get("/info", (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${
+      persons.length
+    } people.</p><p>${new Date()}</p>`
+  );
 });
 
 app.get("/api/persons", (request, response) => {
