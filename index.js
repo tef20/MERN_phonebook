e53a25eb6c283cd90ba67886af69f91445a93c56
Chris Telford <christelford@hotmail.co.uk>
2022-09-23 13:50:29 -0700

    3.1, express app responds to GET

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+
+const persons = {
+  persons: [
+    {
+      name: "Ada Lovelace",
+      number: "39-44-5323523",
+      id: 2,
+    },
+    {
+      name: "Dan Abramov",
+      number: "12121",
+      id: 3,
+    },
+    {
+      name: "Mary Poppendieck",
+      number: "39-23-6423122",
+      id: 4,
+    },
+    {
+      name: "aa",
+      number: "bb",
+      id: 5,
+    },
+  ],
+};
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello</h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+  response.send(persons);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
